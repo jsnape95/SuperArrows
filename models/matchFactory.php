b4ee4eeb5108b1177ecf6e4b2ffa2ea6b571ce29
danielatherton1 <daniel.atherton@skybettingandgaming.com>
2017-09-21 10:31:44 +0100

    danny commit

--- models/matchFactory.php
+++ models/matchFactory.php
@@ -3,29 +3,59 @@
 
 class MatchFactory {
     private $db;
-    
+
     public function __construct(PDO $db){
-        $this->db = $db; 
+        $this->db = $db;
     }
+
+    public function save(Match $match) {
+        $stmt = $this->db->prepare("
+            insert into matches (matchdate, player1, player2, roundsid)
+            values(:matchdate, :player1, :player2, :roundid)
+        ");
+
+        $result = $stmt->execute([
+
+            'matchdate' => $match->matchdate,
+            'player1' => $match->player1,
+            'player2' => $match->player2,
+            'roundsid' => $match->roundid,
+
+        ]);
+
+        $match->id = $this->db->lastInsertId();
+
 
     public function getRoundMatches() {
-        $q = $this->db->query("
+      $s = $this->db->prepare("select player1, player2 from matches where roundsid = :id");
+      $s->execute(['id' => $this->id]);
 
-            select players.firstname as player1first, 
-                    players.lastname as player1last, 
-                    p2.firstname as player2first, 
-                    p2.lastname as player2last 
-            from matches
-            join players 
-            on matches.player1 = players.id
-            join players as p2 
-            on matches.player2 = p2.id
-        ;");
-        return $q;
+      $matches = [];
+      foreach ($s as $row){
+          $matches[] = $row;
+      }
+
+      return $matches;
+
+        //
+        //
+        // $q = $this->db->query("
+        //
+        //     select players.firstname as player1first,
+        //             players.lastname as player1last,
+        //             p2.firstname as player2first,
+        //             p2.lastname as player2last
+        //     from matches
+        //     join players
+        //     on matches.player1 = players.id
+        //     join players as p2
+        //     on matches.player2 = p2.id
+        // ;");
+        // return $q;
     }
 
 
-    
+
 }
 
 ?>

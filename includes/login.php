f42d9f8455c5631cf3ee61a64d1f4de2dca55c0c
danielatherton1 <daniel.atherton@skybettingandgaming.com>
2017-10-17 11:50:03 +0100

    changes

--- includes/login.php
+++ includes/login.php
@@ -1,10 +1,10 @@
 <a style="color: white; display: inline; float: right; white-space: nowrap; overflow:hidden;"></a>
 <form action="/includes/loginform.php" method="post">
     Username:<br />
-    <input type="text" name="username" value="<?php echo $submitted_username; ?>" />
+    <input type="text" name="username" class ="cl-black" class ="txt-right" value="<?php echo $submitted_username; ?>" />
     <br /><br />
     Password:<br />
-    <input type="password" name="password" value="" />
+    <input type="password" name="password" class ="cl-black" class ="txt-right" value="" />
     <br /><br />
-    <input type="submit" value="Login" />
-</form>s
+    <input type="submit" value="Login" class ="cl-black" />
+</form>

8d1e9826df169fb9dcffa144432b08b2f65b8922
preeny95 <preeny@sky.com>
2017-10-19 14:20:44 +0100

    nav bar changed

--- includes/navBar.inc.php
+++ includes/navBar.inc.php
@@ -1,39 +1,38 @@
 <div id="navHeader">
     <div class="container">
         <div class="mart-28 padt-10">
-
+        <div id="logoDiv" class="disp-in padb-10 fs-18">
+                    <a href="../index.php"><img src="../img/logo.jpg" alt="logo" class="center"></a>
+                </div>
         </div>
-        <div class="marl-25p" style="margin-auto;">
-            <ul id="navList" class="list-inline">
+        <div class="disp-block" style="margin-top:4%; margin-left:15%">
+        <ul id="navList" class="list-inline">
             <?
                 if(isset($_SESSION['admin']))
                 {
                     echo "<li><a href='logics/authorize.php'>Admin Page</a></li>";
                 }
             ?>
-                <li><a href='registerform.php'>Register</a></li>
                 <li><a href='generateResults.php'>View Results</a></li>
 
-                <div id="logoDiv" class="disp-in padb-10 fs-18">
-                    <a href="../index.php"><img src="../img/logo.jpg" alt="logo" class="center"></a>
-                </div>
+
 
                 <?
-    if(isset($_GET['failed'])){
-         echo "Login has failed!";
-    }
-    if (isset($_SESSION['user']))
-    {
-        include "includes/logoutbutton.php";
-    }
-    if(isset($_SESSION['admin']))
-    {
-        include "includes/logoutbutton.php";
-    }
-    if(empty($_SESSION))
-    {
-                include "includes/login.php";
-    }
+            if(isset($_GET['failed'])){
+                echo "Login has failed!";
+            }
+            if (isset($_SESSION['user']))
+            {
+                include "includes/logoutbutton.php";
+            }
+            if(isset($_SESSION['admin']))
+            {
+                include "includes/logoutbutton.php";
+            }
+            if(empty($_SESSION))
+            {
+                        include "includes/login.php";
+            }
                 ?>
             </ul>
         </div>

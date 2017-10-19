bcaec96b745e62dd8a1f20ddd90991b5c3ae8fbe
preeny95 <preeny@sky.com>
2017-10-19 15:01:16 +0100

    css change

--- includes/navBar.inc.php
+++ includes/navBar.inc.php
@@ -1,11 +1,11 @@
 <div id="navHeader">
     <div class="container">
-        <div class="mart-28 padt-10">
-        <div id="logoDiv" class="disp-in padb-10 fs-18">
-                    <a href="../index.php"><img src="../img/logo.jpg" alt="logo" class="center"></a>
+        <div class="mart-0 pad-5">
+        <div id="logoDiv" class="disp-in padb-2 fs-4">
+                    <a href="../index.php"><img src="../img/logo2.png" alt="logo" class="center"></a>
                 </div>
         </div>
-        <div class="disp-block" style="margin-top:4%; margin-left:15%">
+        <div class="disp-block" style="margin-top:6%; margin-left:15%">
         <ul id="navList" class="list-inline">
             <?
                 if(isset($_SESSION['admin']))
